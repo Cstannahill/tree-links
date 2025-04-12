7c89b04b5dd55a1b05f90bcba110b3c9e916ee19
Cstannahill <cstannahill10@gmail.com>
2025-04-12 14:03:44 -0500

    fixes

--- dist/index.js
+++ dist/index.js
@@ -5,10 +5,10 @@ const commander_1 = require("commander");
 const default_1 = require("./commands/default");
 const program = new commander_1.Command();
 program
-  .name("tree-me")
-  .description("Generate MDX project structure with tree and links")
-  .argument("[directory]", "Directory to scan", "src")
-  .action((dir) => {
+    .name("tree-me")
+    .description("Generate MDX project structure with tree and links")
+    .argument("[directory]", "Directory to scan", "src")
+    .action((dir) => {
     (0, default_1.handleDefaultCommand)(dir);
-  });
+});
 program.parse();
